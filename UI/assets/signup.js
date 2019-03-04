c52bb9caad43a32f2c40615cf1871d5e5f72e39e
successgilli <successgilli@gmail.com>
2019-03-04 14:41:02 +0100

    added pages and tested fetch which appears to require cors

---
+++ UI/assets/signup.js
@@ -0,0 +1,117 @@
+let headerCenterDiv=document.getElementById('headerCenterDiv');
+let signinDiv=document.getElementById('signinDiv');
+let modalBackground=document.getElementById('modalBackground');
+let loginButton=document.getElementById('submitButton');
+let closeX=document.getElementsByClassName('close');
+let forgotPassword=document.getElementById('forgotPassword');
+let loginName=document.getElementById('loginName');
+let loginPassword=document.getElementById('loginPassword');
+let submitSignup = document.getElementById('submitSignup');
+let signupForm = document.forms['signupForm'];
+
+//signup implementation
+submitSignup.addEventListener('click', (e) => {
+    e.preventDefault();
+    const url = 'https://politico2gilbert.herokuapp.com/api/v1/auth/signup';
+    const signUser = {
+        firstname: signupForm.elements['firstname'].value,
+        lastname: signupForm.elements['lastname'].value,
+        othername: signupForm.elements['othername'].value,
+        userPassword: signupForm.elements['inputPassword'].value,
+        email: signupForm.elements['email'].value,
+        phoneNumber: signupForm.elements['phoneNumber'].value,
+        passportUrl: signupForm.elements['passportUrl'].value
+    }
+    const request = new Request (url, {
+        method: 'POST',
+        headers: new Headers ({'Content-type':'application/json', 'Accept':'application/json,text/plain,*/*'}),
+        body: JSON.stringify(signUser)
+    })
+    fetch(request).then((res) => {
+        if (res.ok){
+            return res;
+        }
+        throw Error (res.statusText);
+    }).then((res) => {
+        console.log(res.json())
+    }).catch (e => console.log(e))
+})
+//end signup implementation
+forgotPassword.addEventListener('click',()=>{
+    document.getElementById('resetPasswordModalBackground').style.display='flex';
+})
+
+modalBackground.addEventListener('click',(e)=>{
+    let target=e.target;
+    console.log(target)
+    //ensure loginForm is not closed when clicked on except the 
+    //click happened on the modal directly
+    if(target===modalBackground){
+        //ensure the loginForm dosnt close when inputs are in the form
+        if(loginName.value.trim()===""&&loginPassword.value.trim()===""){
+            modalBackground.style.display='none';
+            headerCenterDiv.style.backgroundColor='rgb(77, 68, 68)';
+            headerCenterDiv.style.color='grey';
+        
+        }
+        
+    }
+    
+})
+
+
+loginButton.addEventListener('click',(e)=>{
+    e.preventDefault();
+    location='./userPage.html';
+})
+
+headerCenterDiv.addEventListener('click',()=>{
+    
+    if(window.getComputedStyle(modalBackground).getPropertyValue('display')==='none'){
+        if(window.innerWidth>=901){
+            modalBackground.style.display='block';
+            headerCenterDiv.style.backgroundColor='white';
+            headerCenterDiv.style.color='black';
+            document.getElementById('modalContent').style.left=headerCenterDiv.offsetLeft+'px';
+        }else if(window.innerWidth<901 && window.innerWidth>450 ){
+            modalBackground.style.display='block';
+            headerCenterDiv.style.backgroundColor='white';
+            headerCenterDiv.style.color='black';
+            document.getElementById('modalContent').style.left=((headerCenterDiv.offsetLeft +130)-270)+'px';
+        }else{
+            modalBackground.style.display='block';
+            headerCenterDiv.style.backgroundColor='white';
+            headerCenterDiv.style.color='black';
+            document.getElementById('modalContent').style.left='0px';
+        }
+    }else{
+        modalBackground.style.display='none';
+        headerCenterDiv.style.backgroundColor='rgb(77, 68, 68)';
+        headerCenterDiv.style.color='grey';
+    }
+
+      
+})
+
+window.addEventListener('resize',()=>{
+    if(window.getComputedStyle(modalBackground).getPropertyValue('display')!=='none'){
+        if(window.innerWidth>=901){
+    
+            document.getElementById('modalContent').style.left=headerCenterDiv.offsetLeft+'px';
+            headerCenterDiv.style.backgroundColor='white';
+            headerCenterDiv.style.color='black';
+            
+        }
+        else if(window.innerWidth<901 && window.innerWidth>450 ){
+            headerCenterDiv.style.backgroundColor='white';
+            headerCenterDiv.style.color='black';
+            document.getElementById('modalContent').style.left=((headerCenterDiv.offsetLeft +130)-270)+'px';
+        }else{
+            headerCenterDiv.style.backgroundColor='white';
+            headerCenterDiv.style.color='black';
+            document.getElementById('modalContent').style.left='0px';
+        }
+    
+}   
+    
+})
---
+++ UI/assets/userPage.js
@@ -0,0 +1,123 @@
+ let navBar=document.getElementById('navBar');
+ let headerLeft=document.getElementById('headerLeft');
+ let headerRight=document.getElementById('headerRight');
+ let aside=document.getElementById('aside');
+ let main=document.getElementById('main');
+ let asideDiv=document.getElementsByClassName('aside');
+ let modalBackground=document.getElementById('modalBackground');
+ let statementArrows=document.getElementsByClassName('fa');
+let statementDiv=document.getElementsByClassName('statement');
+let blueBoxLinks=document.getElementsByClassName('menu');
+
+
+
+for(i=0;i<blueBoxLinks.length;i++){
+    if(i===0){
+        blueBoxLinks[i].addEventListener('click',()=>{
+            location='./userPageVote.html';
+        })
+    }
+    else if(i===1){
+        blueBoxLinks[i].addEventListener('click',()=>{
+            location='./userRun.html';
+        })
+    }
+    else if(i===2){
+        blueBoxLinks[i].addEventListener('click',()=>{
+            location='./userPageResults.html';
+        })
+    }
+}
+
+window.addEventListener('hashchange',()=>{
+    window.scrollTo(0,window.scrollY-70);
+})
+
+window.addEventListener('scroll',()=>{    
+        for(i=0;i<asideDiv.length;i++){
+            asideDiv[i].style.top=window.pageYOffset+'px';
+        }   
+}) 
+window.addEventListener('load',()=>{
+    for(i=0;i<statementArrows.length;i++){
+        statementArrows[i].innerHTML='&#xf107;';    
+    }   
+})
+for(i=0;i<statementArrows.length;i++){
+    console.log(statementArrows);
+    statementArrows[i].addEventListener('click',(e)=>{
+        let target=e.target;
+        let realTarget=target.parentNode.nextElementSibling;
+        let state= window.getComputedStyle(realTarget).getPropertyValue('display');
+        if(state==='none'){
+            for(i=0;i<statementArrows.length;i++){
+                statementArrows[i].innerHTML='&#xf107;';
+                statementDiv[i].style.display='none';
+            }
+            target.innerHTML='&#xf106;';
+            realTarget.style.display='block';
+        }
+        else{
+            target.innerHTML='&#xf107;';
+            realTarget.style.display='none';
+        }
+
+    })
+}
+window.addEventListener('resize',()=>{
+    if(window.innerWidth<=765){
+       aside.style.marginLeft='-170px';
+       headerLeft.style.marginLeft='-170px';
+       main.style.flexBasis='100%';
+       headerRight.style.flexBasis='100%';
+       modalBackground.style.display='none';
+    }
+    if(window.innerWidth>765){
+        aside.style.marginLeft='0px';
+        headerLeft.style.marginLeft='0px';
+        main.style.flexBasis='80%';
+        headerRight.style.flexBasis='80%';
+        modalBackground.style.display='none';
+     }
+})
+modalBackground.addEventListener('click',()=>{
+    aside.style.marginLeft='-170px';   
+    headerLeft.style.marginLeft='-170px'; 
+    modalBackground.style.display='none';
+})
+navBar.addEventListener('click',()=>{
+    if(window.innerWidth>=766){
+        
+        if(window.getComputedStyle(aside).getPropertyValue('margin-left')==='0px'){
+            let width=window.getComputedStyle(aside).getPropertyValue('width');
+            main.style.flexBasis='100%';
+            headerRight.style.flexBasis='100%';
+            aside.style.marginLeft='-'+width;
+            headerLeft.style.marginLeft='-'+width;
+    
+        }else{
+            
+            aside.style.marginLeft='0px';
+            main.style.flexBasis='80%';
+            headerLeft.style.marginLeft='0px';
+            headerRight.style.flexBasis='80%';
+        }
+    }
+    //response when screen is samller than Tablet
+    if(window.innerWidth<766){
+        
+        if(window.getComputedStyle(aside).getPropertyValue('margin-left')==='-170px'){
+            aside.style.marginLeft='0px';
+            headerLeft.style.marginLeft='0px';
+            modalBackground.style.display='block';
+        }else{
+            aside.style.marginLeft='-170px';   
+            headerLeft.style.marginLeft='-170px'; 
+            modalBackground.style.display='none';
+        }
+    }
+
+})
+
+ 
+ 
